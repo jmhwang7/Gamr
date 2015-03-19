98c2839019482623e51863f6bf308af65ffcfb72
Jennifer Hwang <jmhwang7@gmail.com>
2015-03-19 15:57:55 -0400

    Updates location on the server. Things that need to be changed in the future: - When it's done: AsyncTask is currently automatically started upon entering FindGamesActivity. - Hardcoded user id: When we have user generation working, we can change this. - What happens when update succeeds or fails: Currently, the result is just logged. We can override onPostExecute to better handle this. - What happens when connection to Google Play services fails or is suspended: No error handling for this currently.

--- app/src/main/java/com/gamr/gamr/FindGamesActivity.java
+++ app/src/main/java/com/gamr/gamr/FindGamesActivity.java
@@ -1,6 +1,12 @@
 package com.gamr.gamr;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,14 +17,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.gamr.gamr.AsyncTasks.UpdateLocationTask;
 import com.gamr.gamr.FindGamesFragments.MatchesFragment;
 import com.gamr.gamr.FindGamesFragments.MessagesFragment;
 import com.gamr.gamr.FindGamesFragments.NearYouFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.Locale;
 
 
-public class FindGamesActivity extends ActionBarActivity {
+public class FindGamesActivity extends ActionBarActivity
+    implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+    private static final String LOG_TAG = FindGamesActivity.class.getSimpleName();
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private static final String DIALOG_ERROR = "dialog_error";
+    private boolean mResolvingError = false;
+
     public static final int MATCHES_FRAGMENT_NUMBER = 0;
     public static final int NEAR_YOU_FRAGMENT_NUMBER = 1;
     public static final int MESSAGES_FRAGMENT_NUMBER = 2;
@@ -40,10 +58,16 @@ public class FindGamesActivity extends ActionBarActivity {
      */
     ViewPager mViewPager;
 
+    /**
+     * The (@link GoogleApiClient} that provides a way to access Google Play services
+     */
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        buildGoogleApiClient();
         setContentView(R.layout.activity_find_games);
 
         // Create the adapter that will return a fragment for each of the three
@@ -87,7 +111,6 @@ public class FindGamesActivity extends ActionBarActivity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -110,6 +133,113 @@ public class FindGamesActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    /**
+     * Creates instance of Google API Client to access Google Play Services with.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (location != null) {
+            UpdateLocationTask locationTask = new UpdateLocationTask();
+            locationTask.execute(location.getLatitude(), location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+        }
+    }
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((FindGamesActivity)getActivity()).onDialogDismissed();
+        }
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
--- web/functions/update_location.php
+++ web/functions/update_location.php
@@ -3,6 +3,8 @@ function update_location($db, $user_id, $lat, $lon) {
     $db->query('UPDATE users
     			SET lat=' .$lat. ', lon=' .$lon. '
     			WHERE id="' .$user_id. '";');
-    outputResponse(array('user_id' => $db->insertId()));
+    // http_response_code(200);
+    outputResponse(array('affected_rows' => $db->affectedRows()));
+    // outputResponse(array('user_id' => $db->insertId()));
 }
 ?>
