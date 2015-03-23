3ab2a4f5fa463a5e63a7723703fe7c06d1e79e04
Thomas Cornish <tdcornish@att.net>
2015-03-22 20:46:20 -0400

    Send message works now. Not entirely certain which change fixed it but it seems to be working now

--- app/src/main/java/com/gamr/gamr/ServerRepresentations/Server.java
+++ app/src/main/java/com/gamr/gamr/ServerRepresentations/Server.java
@@ -3,12 +3,12 @@ package com.gamr.gamr.ServerRepresentations;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -21,24 +21,23 @@ import java.util.List;
  * Created by Thomas on 3/21/2015.
  */
 public class Server {
-    private static final String LOG_TAG = Server.class.getSimpleName();
-
     // Base URL
     public static final String BASE_URL = "http://gamr.buildism.net/api1/";
-
-    // Endpoints
-    private static final String GET_MESSAGE_FUNCTION = "get_messages?";
-    private static final String SEND_MESSAGE_FUNCTION = "send_message?";
-    private static final String GET_MATCHES_FUNCTION = "match?";
-
     // Query Params
     public static final String USER_ID_PARAM = "&user_id=";
     public static final String OTHER_USER_ID_PARAM = "&other_user_id=";
     public static final String BEFORE_TIME_PARAM = "&before=";
     public static final String USE_GAMES_PARAM = "&use_games=";
     public static final String USE_LOCATION_PARAM = "&use_location=";
+    private static final String LOG_TAG = Server.class.getSimpleName();
+    // Endpoints
+    private static final String GET_MESSAGE_FUNCTION = "get_messages?";
+    private static final String SEND_MESSAGE_FUNCTION = "send_message?";
+    private static final String GET_MATCHES_FUNCTION = "match?";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        List<Message> convo = getConversation("d49f9b92-b927-11e4-847c-8bb5e9000003", "d49f9b92-b927-11e4-847c-8bb5e9000002");
+        System.out.println(convo.get(0));
         String messageId = sendMessage("d49f9b92-b927-11e4-847c-8bb5e9000003", "d49f9b92-b927-11e4-847c-8bb5e9000002", "Yay, message sending works!");
         System.out.println(messageId);
     }
@@ -52,7 +51,8 @@ public class Server {
             e.printStackTrace();
             return new ArrayList<Message>();
         }
-        List<Message> conversation = new Gson().fromJson(response, new TypeToken<List<Message>>() {}.getType());
+        List<Message> conversation = new Gson().fromJson(response, new TypeToken<List<Message>>() {
+        }.getType());
         return conversation;
     }
 
@@ -65,11 +65,12 @@ public class Server {
             e.printStackTrace();
             return new ArrayList<Match>();
         }
-        List<Match> matches = new Gson().fromJson(response, new TypeToken<List<Match>>() {}.getType());
+        List<Match> matches = new Gson().fromJson(response, new TypeToken<List<Match>>() {
+        }.getType());
         return matches;
     }
 
-    public static String sendMessage(String fromUserId, String toUserId, String text){
+    public static String sendMessage(String fromUserId, String toUserId, String text) {
         try {
             String messageId = post(SEND_MESSAGE_FUNCTION, fromUserId, toUserId, text);
             return messageId;
@@ -82,7 +83,7 @@ public class Server {
     private static String get(String... params) throws IOException {
         URL url = createUrl(params);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        return readResponseStream(connection);
+        return readResponseStream(connection.getInputStream());
     }
 
     private static String post(String... params) throws IOException {
@@ -91,28 +92,26 @@ public class Server {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         //Identify as a POST
-        connection.setRequestMethod("POST");
         connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-        String text = params[3];
-
+        String text = "text=" + params[3];
         //Set content length header
-        byte[] textInBytes = text.getBytes("UTF-8");
-        System.out.println(text + " " + textInBytes.length);
-        connection.setFixedLengthStreamingMode(textInBytes.length);
+        connection.setFixedLengthStreamingMode(text.getBytes().length);
 
         //Open connection and write text
-        BufferedOutputStream post = new BufferedOutputStream(connection.getOutputStream());
-        post.write(textInBytes);
+        PrintWriter post = new PrintWriter(connection.getOutputStream());
+        post.write(text);
+        post.close();
 
-        return readResponseStream(connection);
+        return readResponseStream(connection.getInputStream());
     }
 
-    private static String readResponseStream(HttpURLConnection connection) throws IOException {
-        BufferedReader responseReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    private static String readResponseStream(InputStream responseStream) throws IOException {
+        BufferedReader responseReader = new BufferedReader(new InputStreamReader(responseStream));
         StringBuilder response = new StringBuilder();
-        String output;
-        while ((output = responseReader.readLine()) != null) {
+        for (String output; (output = responseReader.readLine()) != null;) {
             response.append(output);
         }
 
