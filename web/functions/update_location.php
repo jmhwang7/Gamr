2f57d6fcfa6dec1906c21fd6a56e300718285e49
Jennifer Hwang <jmhwang7@gmail.com>
2015-03-23 11:22:51 -0400

    Removed unnecessary commented out code in update_location.php

--- web/functions/update_location.php
+++ web/functions/update_location.php
@@ -3,8 +3,7 @@ function update_location($db, $user_id, $lat, $lon) {
     $db->query('UPDATE users
     			SET lat=' .$lat. ', lon=' .$lon. '
     			WHERE id="' .$user_id. '";');
-    // http_response_code(200);
+
     outputResponse(array('affected_rows' => $db->affectedRows()));
-    // outputResponse(array('user_id' => $db->insertId()));
 }
 ?>
