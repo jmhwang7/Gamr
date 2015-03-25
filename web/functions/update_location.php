e82edf24eedf5ba1c3f8b98f7cad7d24b43e8b3d
Jacob Morgan <jacobmorgan715@gmail.com>
2015-03-25 12:36:02 -0400

    Updated update_location to insert a new user if the ID doesn't already exist

--- web/functions/update_location.php
+++ web/functions/update_location.php
@@ -1,8 +1,14 @@
 <?php
 function update_location($db, $user_id, $lat, $lon) {
-    $db->query('UPDATE users
+    $userExists = $db->query('SELECT id FROM users WHERE id="'.$user_id.'"')->num_rows == 1;
+    if($userExists) {
+        $db->query('UPDATE users
     			SET lat=' .$lat. ', lon=' .$lon. '
-    			WHERE id="' .$user_id. '";');
+    			WHERE id="' .$user_id. '";');        
+    } else {
+        $db->query('INSERT INTO users(id, lat, lon) VALUES("'.$user_id.'", "'.$lat.'", "'.$lon.'")');
+    }
+
 
     outputResponse(array('affected_rows' => $db->affectedRows()));
 }
