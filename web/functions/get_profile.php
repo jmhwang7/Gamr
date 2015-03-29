6d6156ed74392fabca4502eec7f766bc6ec11fcd
Jacob Morgan <jacobmorgan715@gmail.com>
2015-03-29 15:30:22 -0400

    Updated get_profile format again

--- web/functions/get_profile.php
+++ web/functions/get_profile.php
@@ -6,10 +6,9 @@ function get_profile($db, $user_id) {
     $user = $db->queryRow('SELECT * FROM users WHERE id="'.$user_id.'"');
     $output['username'] = $user['username'];
     
-    $output['games'] = array();
     $result = $db->query('SELECT * FROM user_games WHERE user_id="'.$user_id.'"');
     while($row = $result->fetch_assoc()) {
-        $output['games'][$row['game_id']] = array(
+        $output[$row['game_id']] = array(
             'in_game_name' => $row['in_game_name'],
         );
     }
@@ -19,7 +18,7 @@ function get_profile($db, $user_id) {
         if($row['game_id'] == GAME_LOL && $row['field_id'] == FIELD_LOL_RANK) {
             $row['field_value'] = $LOL_RANKS[$row['field_value']];
         }
-        $output['games'][$row['game_id']][$row['field_id']] = $row['field_value'];
+        $output[$row['game_id']][$row['field_id']] = $row['field_value'];
     }
     
     outputResponse($output);
