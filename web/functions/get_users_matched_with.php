fcd82b1b01aef2fea26e33b45e2fb67fa41671f7
Thomas Cornish <tdcornish@att.net>
2015-04-26 17:31:44 -0400

    change to have get_users_matched_with return username and id

--- web/functions/get_users_matched_with.php
+++ web/functions/get_users_matched_with.php
@@ -1,15 +1,18 @@
 <?php
 function get_users_matched_with($db, $gcpm, $user_id) {
-	$results = $db->query('SELECT user_id FROM matches WHERE other_user_id = "'.$user_id.'" AND matched = 1');
+	$results = $db->query('SELECT user_id as id, username FROM matches JOIN users ON matches.user_id = users.id WHERE other_user_id = "'.$user_id.'" AND matched = 1');
 	$matches = array();
 	while($row = $results->fetch_assoc()) {
-		$matches[] = $row['user_id'];
+		$matches[] = $row['id'];
 	}
 
-	$results = $db->query('SELECT other_user_id FROM matches WHERE user_id = "'.$user_id.'" AND matched = 1');
+	$results = $db->query('SELECT other_user_id as id, username FROM matches JOIN users ON matches.other_user_id = users.id WHERE user_id = "'.$user_id.'" AND matched = 1');
 	$other_matches = array();
 	while($row = $results->fetch_assoc()) {
-		$other_matches[] = $row['other_user_id'];
+		$other_matches[] = array(
+			'id' => $row['id'],
+			'username' => $row['username']
+		);
 	}
 	
 	$matches = array_intersect($matches, $other_matches);
