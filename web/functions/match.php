0539084849736830d77f97b6ecdca8f87a71e356
Jennifer Hwang <jmhwang7@gmail.com>
2015-04-30 09:11:13 -0400

    Added number of users matched to the GCM notification

--- web/functions/match.php
+++ web/functions/match.php
@@ -102,6 +102,7 @@ function match($db, $gcpm, $user_id, $use_location, $use_games, $count) {
         return $user['username'];
     }
     $other_usernames = array_map("username", $users);
+    array_unshift($other_usernames, count($other_usernames))
     $gcpm->setDevices($user['registration_id']);
     $response = $gcpm->send($messageType, $other_usernames);
 
